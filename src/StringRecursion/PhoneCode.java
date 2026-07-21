11100309bd7f7aee7df5119021764f691793a17a
harshitrajput1909 <harshitrajput.hk@gmail.com>
2026-07-20 22:16:59 -0700

    phone number dial

--- src/StringRecursion/PhoneCode.java
+++ src/StringRecursion/PhoneCode.java
@@ -1,8 +1,12 @@
 package StringRecursion;
 
+import java.util.ArrayList;
+
 public class PhoneCode {
     static void main() {
-    decode("","12");
+//   ArrayList<String> finallist= decodeArray("","12");
+//        System.out.println(finallist);
+        System.out.println(count("","12"));
     }
     static  void decode(String p,String up){
         if(up.isEmpty()){
@@ -15,5 +19,36 @@ public class PhoneCode {
             decode(p+a,up.substring(1));
 
         }
+    }static ArrayList<String> decodeArray(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list =new ArrayList<>();
+            list.add(p);
+//            System.out.println(p);
+            return list;
+        }
+        ArrayList<String> ans=new ArrayList<>();
+        int ch =up.charAt(0)-'0';
+        for (int i = (ch-1)*3; i <ch*3 ; i++) {
+            char a=(char) ('a'+i);
+            ans.addAll(decodeArray(p+a,up.substring(1)));
+
+        }
+        return ans;
+    }static int  count(String p, String up){
+        if(up.isEmpty()){
+//            ArrayList<String> list =new ArrayList<>();
+//            list.add(p);
+//          System.out.println(p);
+            return 1;
+        }
+//        ArrayList<String> ans=new ArrayList<>();
+        int count =0;
+        int ch =up.charAt(0)-'0';
+        for (int i = (ch-1)*3; i <ch*3 ; i++) {
+            char a=(char) ('a'+i);
+            count=count+(count(p+a,up.substring(1)));
+
+        }
+        return count;
     }
 }
